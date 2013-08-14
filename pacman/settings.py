b00fab379d1474cea64f1723556680495fcd0bc2
Luis Fagundes <lhfagundes@hacklab.com.br>
2013-08-14 13:45:57 -0300

    adjustments for proper packing

--- pacman/settings.py
+++ pacman/settings.py
@@ -5,7 +5,7 @@ ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 DOWNLOAD_TMP_DIR = os.path.join(ROOT, 'tmp')
 REPOSITORY_PUBLIC_KEY = os.path.join(ROOT, 'repository.pub')
 LOCAL_REPOSITORY_DIR = '/pkgs' 
-HTML_DIR = os.path.join(ROOT, 'html')
+HTML_DIR = '/usr/share/mod-pacmanager/html/'
 REPOSITORY_ADDRESS = 'packages.portalmod.com'
 PORT = 8889
 
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+setup(name = 'mod-pacmanager',
+      version = '0.1',
+      description = 'MOD ArchLinux Package Manager',
+      long_description = 'MOD - Musician Operated Device - ArchLinux Package Manager',
+      author = "Hacklab and AGR",
+      author_email = "lhfagundes@hacklab.com.br",
+      license = "GPLv3",
+      packages = find_packages(),
+      entry_points = {
+          'console_scripts': [
+              'mod-pacmanager = pacman.webserver:run',
+              ]
+          },
+      scripts = [
+      ],
+      data_files = [("/usr/share/mod-pacmanager/", ['repository.pub'])],
+      install_requires = ['tornado'],
+      classifiers = [
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+        ],
+      url = 'http://portalmod.com/',
+)
