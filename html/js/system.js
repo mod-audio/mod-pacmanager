251aca1a2e0126de5bbff5cb09409b24bb7948a3
Luis Fagundes <lhfagundes@hacklab.com.br>
2013-09-03 02:18:36 -0300

    restoring hardcoded default repository in system.js

--- html/js/system.js
+++ html/js/system.js
@@ -24,7 +24,7 @@ function Installer(options) {
 
     options = $.extend({
 	// This is usually defined, but let's have it hardcoded here anyway
-	repository: REPOSITORY, 
+	repository: 'http://packages.portalmod.com/api', 
 	// The address of the server where packages will be installed. Empty is
 	// ok if the interface is being served by the package server
 	localServer: '',
